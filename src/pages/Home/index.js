62b9aca01fbb090bb2fe1e6f09e514afc6c75b8e
andgmartins <and.gmartins@gmail.com>
2019-11-07 13:00:39 -0300

    Modulo 07 Coninue

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,71 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdAddShoppingCart } from 'react-icons/md';
+import { ProductList } from './styles';
 
 export default function Home() {
-    return <h1>Home</h1>;
+    return (
+        <ProductList>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/chuteira-campo-juvenil-adidas-nemeziz-19-3-fg/10/COL-7813-010/COL-7813-010_detalhe1.jpg?resize=280:280"
+                    alt="Chuteira"
+                />
+                <strong>Chuteira</strong>
+                <span>R$ 200,00</span>
+
+                <button>
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>Adicionar ao Carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/chuteira-campo-juvenil-adidas-nemeziz-19-3-fg/10/COL-7813-010/COL-7813-010_detalhe1.jpg?resize=280:280"
+                    alt="Chuteira"
+                />
+                <strong>Chuteira</strong>
+                <span>R$ 200,00</span>
+
+                <button>
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>Adicionar ao Carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/chuteira-campo-juvenil-adidas-nemeziz-19-3-fg/10/COL-7813-010/COL-7813-010_detalhe1.jpg?resize=280:280"
+                    alt="Chuteira"
+                />
+                <strong>Chuteira</strong>
+                <span>R$ 200,00</span>
+
+                <button>
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>Adicionar ao Carrinho</span>
+                </button>
+            </li>
+            <li>
+                <img
+                    src="https://static.netshoes.com.br/produtos/chuteira-campo-juvenil-adidas-nemeziz-19-3-fg/10/COL-7813-010/COL-7813-010_detalhe1.jpg?resize=280:280"
+                    alt="Chuteira"
+                />
+                <strong>Chuteira</strong>
+                <span>R$ 200,00</span>
+
+                <button>
+                    <div>
+                        <MdAddShoppingCart size={16} color="#FFF" /> 3
+                    </div>
+                    <span>Adicionar ao Carrinho</span>
+                </button>
+            </li>
+        </ProductList>
+    );
 }
